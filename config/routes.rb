8c1831fc0cf5ea5fe814bd15524105172a90fcc9
laplacia <iageas7@gmail.com>
2018-08-18 11:37:11 +0000

    Add top

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root 'books#top'
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   delete '/books/:id' => 'books#destroy', as: 'destroy_book'
